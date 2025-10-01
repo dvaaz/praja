efb1f16f0af20dfe461516079a26f830e0e6f383
darlEuMesmo <darley.azevedo@gmail.com>
2025-09-30 22:28:53 -0300

    primeiro commit

---
+++ src/main/java/br/senac/rj/edumysql/praJa/PraJaApplication.java
@@ -0,0 +1,13 @@
+package br.senac.rj.edumysql.praJa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PraJaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PraJaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/senac/rj/edumysql/praJa/PraJaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.senac.rj.edumysql.praJa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PraJaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
